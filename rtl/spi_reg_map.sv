c99a158304aed1e820496be533dfaecddc73bf30
ArjunG12 <46287498+ArjunG12@users.noreply.github.com>
2026-05-10 04:29:31 +0530

    Update spi_reg_map.sv

--- rtl/spi_reg_map.sv
+++ rtl/spi_reg_map.sv
@@ -12,13 +12,20 @@ module spi_reg_map (
 
     logic [31:0] status_reg;
     logic [31:0] rw_regs [1:31];
-
+    logic [1:0] write_en_pulse_sys_buff;
     assign ff = |status_reg;
 
     assign reg_read_data = (rx_addr == 6'd0)  ? status_reg :
                            (rx_addr <= 6'd31) ? rw_regs[rx_addr] :
                                                 32'hFEDCBA98;
-
+    
+    always_ff @(posedge sys_clk,negedge sys_rst_n)begin
+        if(!sys_rst_n)write_en_pulse_sys_buff<=0;
+        else begin
+            write_en_pulse_sys_buff<={write_en_pulse_sys_buff[0],write_en_pulse_sys};
+        end
+    end
+    
     always_ff @(posedge sys_clk or negedge sys_rst_n) begin
         if (!sys_rst_n) begin
             status_reg <= 32'h0;
@@ -29,7 +36,7 @@ module spi_reg_map (
         else begin
             // 1. Status Register (Address 0)
             // HW faults are ALWAYS accumulated, regardless of SPI state
-            if (!spi_active_sync && write_en_pulse_sys && (rx_addr == 6'd0)) begin
+            if (!spi_active_sync && write_en_pulse_sys_buff[1] && (rx_addr == 6'd0)) begin
                 // W1C logic COMBINED with Hardware Set
                 status_reg <= (status_reg & ~rx_data) | hw_fault_in;
             end else begin
@@ -38,7 +45,7 @@ module spi_reg_map (
             end
 
             // 2. RW Registers (Addresses 1 to 31) - frozen during SPI active
-            if (!spi_active_sync && write_en_pulse_sys && (rx_addr > 6'd0) && (rx_addr < 6'd32)) begin
+            if (write_en_pulse_sys_buff[1] && (rx_addr > 6'd0) && (rx_addr < 6'd32)) begin
                 rw_regs[rx_addr] <= rx_data;
             end
         end
