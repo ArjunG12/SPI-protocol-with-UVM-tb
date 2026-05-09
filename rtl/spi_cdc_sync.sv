24dee58c4b87738c859a394c4fb7de146095c0c7
ArjunG12 <46287498+ArjunG12@users.noreply.github.com>
2026-05-10 04:28:43 +0530

    Update spi_rst_n assignment in spi_cdc_sync.sv
    Modified the assignment of spi_rst_n to only depend on sys_rst_n.

--- rtl/spi_cdc_sync.sv
+++ rtl/spi_cdc_sync.sv
@@ -13,7 +13,7 @@ module spi_cdc_sync (
   logic spi_rst_n;
   logic pi_q, sys_q1, sys_q2, sys_q3;
   
-  assign spi_rst_n= sys_rst_n & ~pi_csn;
+  assign spi_rst_n= sys_rst_n ;
   
   always_ff@(posedge pi_sck, negedge spi_rst_n)begin
     if(!spi_rst_n)pi_q<=0;
